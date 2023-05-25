1ae604a1bfc5c3c79792bda334057e294295f3c2
Sivaramanraja98 <sivaram1194@gmail.com>
2023-05-25 22:00:21 +0530

    first commit

---
+++ src/components/ScrollToTop.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react'
+
+const ScrollToTop = () => {
+  const [showTopBtn, setShowTopBtn] = useState(false)
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 400) {
+        setShowTopBtn(true)
+      } else {
+        setShowTopBtn(false)
+      }
+    })
+  }, [])
+  const goToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    })
+  }
+  return (
+    <div>
+      {showTopBtn && (
+        <a
+          className="scroll-to-top rounded"
+          style={{ display: 'inline' }}
+          onClick={goToTop}
+        >
+          <i className="fas fa-angle-up"></i>
+        </a>
+      )}
+    </div>
+  )
+}
+
+export default ScrollToTop
